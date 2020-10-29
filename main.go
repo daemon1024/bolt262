8332c1abcb206eabe7fe02ec1350acd6d0aa0f70
daemon1024 <barun1024@gmail.com>
2020-10-29 20:17:42 +0530

    optimisation
    reduce unneccesary read of must includes
    
    use actual temp files instead of writing tmp.js
    
    colored output for counter

--- main.go
+++ main.go
@@ -25,37 +25,38 @@ func main() {
 	if fi.Mode().IsDir() {
 		walkDir(path)
 	} else {
-		processFile(path)
+		// processFile(path)
+		log.Print("commented process")
 	}
 }
 
 func walkDir(rootPath string) {
-	path := rootPath + "test/"
+	path := rootPath
+	assert, err := ioutil.ReadFile(os.Args[2] + "assert.js")
+	if err != nil {
+		log.Print(err)
+	}
+	sta, err := ioutil.ReadFile(os.Args[2] + "sta.js")
+	if err != nil {
+		log.Print(err)
+	}
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
 		if info.IsDir() == false {
-			processFile(path)
+			processFile(path, assert, sta)
 		}
 		return nil
 	})
-	fmt.Printf("Total files %d, Passed Tests %d, Failed Tests %d", count, pass, fail)
+	fmt.Printf("\033[36mTotal files %d\n\033[0m\033[31mFailed Tests %d\n\033[0m\033[34mPassed Tests %d\033[0m", count, fail, pass)
 }
 
-func processFile(path string) {
+func processFile(path string, assert []byte, sta []byte) {
 	data, _ := ioutil.ReadFile(path)
 	fmt.Println("\033[36m", path)
 	var includeFinal []byte
-	assert, err := ioutil.ReadFile(os.Args[2] + "assert.js")
-	if err != nil {
-		log.Print(err)
-	}
 	includeFinal = append(includeFinal, assert...)
-	sta, err := ioutil.ReadFile(os.Args[2] + "sta.js")
-	if err != nil {
-		log.Print(err)
-	}
 	includeFinal = append(includeFinal, sta...)
 	re, _ := regexp.Compile(`\/\*---([\s\S]*?)---\*\/`)
 	y := re.FindSubmatch([]byte(data))
@@ -76,14 +77,29 @@ func processFile(path string) {
 	var finalFile []byte
 	finalFile = append(finalFile, includeFinal...)
 	finalFile = append(finalFile, data...)
-	currDir, _ := os.Getwd()
-	err = ioutil.WriteFile(currDir+"/tmp.js", finalFile, 0777)
+	// currDir, _ := os.Getwd()
+	// err = ioutil.WriteFile(currDir+"/tmp.js", finalFile, 0777)
+	// if err != nil {
+	// 	log.Print(err)
+	// }
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "tmptest-")
 	if err != nil {
-		log.Print(err)
+		log.Fatal("Cannot create temporary file", err)
 	}
+
+	// Remember to clean up the file afterwards
+	defer os.Remove(tmpFile.Name())
+	count++
+	fmt.Println("Created File: " + tmpFile.Name())
+
+	// Example writing to the file
+	if _, err = tmpFile.Write(finalFile); err != nil {
+		log.Fatal("Failed to write to temporary file", err)
+	}
+
 	var outbuf, errbuf bytes.Buffer
 	var exitCode int
-	cmd := exec.Command("node tmp.js")
+	cmd := exec.Command("node", tmpFile.Name())
 	cmd.Stdout = &outbuf
 	cmd.Stderr = &errbuf
 
@@ -109,5 +125,8 @@ func processFile(path string) {
 	}
 	fmt.Println("\033[31m", stderr)
 	fmt.Println("\033[36m", exitCode)
-	count++
+	// Close the file
+	if err := tmpFile.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
